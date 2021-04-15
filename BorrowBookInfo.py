bec306c59d81bc47b94a6852c54785059467f573
Prodraft <prodraft@naver.com>
2021-04-15 17:48:59 +0900

    first commit

---
+++ BorrowBookInfo.py
@@ -0,0 +1,35 @@
+import requests
+import urllib.parse
+import json
+
+ # [0] 대출가능여부 ㅣ [1] 책위치(몇층인지) ㅣ [2] 책위치(번호) ㅣ [3] 대출자학번(가림) ㅣ [4] 대출자이름(가림) ㅣ [5] 도서 반납 예정일 ㅣ [6] 대출자학번 ㅣ [7] 대출자이름
+def SerchBorrowInfo(cno):
+  url = "http://library.honam.ac.kr/cheetah/api/detail?Cno="+cno
+  req = requests.get(url)
+  req = json.loads(req.text)
+
+  borrowInfo = []
+  try:
+    for borrow in req["RnoList"]:
+      temp = []
+      CFType = borrow["CFType"]                 # 대출 가능 여부
+      Position = borrow["Position"]             # 도서관내 책 위치(층 정보 ex 2층 문학자료실)
+      CallNumber =borrow["CallNumber"]          # 도서관내 책 위치(책 정보 ex 813.6 이39ㄷ)
+      LentUser = borrow["LentUesr"]             # 도서 대출자 학번(일부 가림형태)
+      LentUserName = borrow["LentUesrName"]     # 도서 대출자 이름(일부 가림형태)
+      Period = borrow["Period"]                 # 도서 반납 예정일
+      RnoStatus = borrow["RnoStatus"]           # 책 대출정보&반납일&대출자학번&대출자이름
+      stamnummer = RnoStatus[RnoStatus.find("=")+1:RnoStatus.rfind("=")]
+      borrower = RnoStatus[RnoStatus.rfind("=")+1:]
+      temp.append(CFType)
+      temp.append(Position)
+      temp.append(CallNumber)
+      temp.append(LentUser)
+      temp.append(LentUserName)
+      temp.append(Period)
+      temp.append(stamnummer)
+      temp.append(borrower)
+      borrowInfo.append(temp)
+  except KeyError:
+    print("대출정보가 없는 책입니다. 같은 이름의 책이 여러개일 경우 다른책을 선택하세요.")
+  return borrowInfo
---
+++ SerchBook.py
@@ -0,0 +1,53 @@
+import requests
+import urllib.parse
+import json
+
+# 사용자가 입력한 책이 호남대학교 도서관에 소장중인지 확인하는 함수
+def SearchLibraryBookPossession(bookTitle):
+  # api 주소
+  api = "http://library.honam.ac.kr/cheetah/api/search?"
+  # 세부검색 설정
+  values = {
+    "otwa1":"T",        # T = 서명 / TK = 서명 키워드 / A = 저자명 / AK = 저자명 키워드 / P = 출판사 
+                        # IDX = 통합 / S = 주제명 / SK = 주제명 키워드 / X = 총서명 / XK = 총서명 키워드 / N = ISBN,ISSN / NC = 청구기호 / R = 등록번호 
+    "otod1": bookTitle, # 검색할 문자열
+    "otbool1":"A",      # A = AND / O = OR  N = NOT
+    "otpn1":"K",        # K = 키워드 / L = 전방일치 / M = 완전일치
+    "sp":"1"            # 1 페이지 검색결과
+
+    ########################################
+    # << 여러 조건으로 검색할때 사용 / 추후 업데이트 예정 >>
+    # "otwa2":"A",
+    # "otod2": "",
+    # "otbool2":"A",
+    # "otpn2":"K",.
+
+    # "otwa3":"P",   
+    # "otod3": "",
+    # "otbool3":"A",
+    # "otpn3":"K",
+  #########################################
+  }
+  params = urllib.parse.urlencode(values)
+  url = api + params
+  req = requests.get(url)
+  req = json.loads(req.text)
+  bookSearchData = [] # 검색결과 저장
+  for search in req["ListItem"]["BasicItem"]:
+    temp = []
+    bookTitle = search["Title"]           # 책 제목
+    author = search["Author"]             # 작가
+    publisher = search["Publisher"]       # 출판사
+    publishYear = search["PublishYear"]   # 책 출판년도
+    # isbn = search["ISBN"]               # 책 ISBN
+    # callNumber = search["CallNumber"]   # 도서관 책 위치
+    cno = search["Cno"]                   # 호대 책관리 번호
+    temp.append(bookTitle)
+    temp.append(author)
+    temp.append(publisher)
+    temp.append(publishYear)
+    # temp.append(isbn)
+    # temp.append(callNumber)
+    temp.append(cno)
+    bookSearchData.append(temp)
+  return bookSearchData
