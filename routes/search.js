a725237044adf3bc42adb650033fa34d20e6397c
adx <nogo22@naver.com>
2021-04-19 04:36:36 +0900

    남은 구현 기능 : 검색 결과 데이터 -> 페이지 반영

--- routes/search.js
+++ routes/search.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const app = express();
 const { PythonShell } = require("python-shell");
 
-let book_cnt;
-let title = "1984";
+let book_cnt = null;
+let bookimg = null;
+let title = null;
 let writer = "조지 오웰";
 let publisher = "민음사";
 let location = "2층 소설";
 let state = "대출 불가";
 
-ConvertType = (type) => {
+convertType = (type) => {
   switch (type) {
     case "저자명":
       return "A";
@@ -21,11 +23,37 @@ ConvertType = (type) => {
   }
 };
 
-router.get("/", (req, res, next) => {
+putData = (data, index) => {
+  let dataindex = index % 6;
+
+  switch (dataindex) {
+    case 0:
+      bookimg = data;
+      break;
+    case 1:
+      title = data;
+      break;
+    case 2:
+      writer = data;
+      break;
+    case 3:
+      publisher = data;
+      break;
+    case 4:
+      location = data;
+      break;
+    case 5:
+      state = data;
+      break;
+      deafualt: break;
+  }
+};
+
+router.get("/", async (req, res, next) => {
   const previous_keyword = req.query.keyword;
   const INPUT = req.query.keyword;
   const TYPE = req.query.option;
-  const CONVERTTYPE = ConvertType(TYPE);
+  const CONVERTTYPE = convertType(TYPE);
   console.log("INPUT: ", INPUT);
   console.log("CONVERTTYPE: ", CONVERTTYPE);
 
@@ -42,7 +70,7 @@ router.get("/", (req, res, next) => {
     option,
     (err, res) => {
       if (err) throw err;
-      bookCnt = 0;
+      bookCnt = 2;
       let i = 0;
       let j = 0;
       while (res[i] !== undefined) {
@@ -51,6 +79,7 @@ router.get("/", (req, res, next) => {
         for (j = i; j < i + 6; j++) {
           console.log(j);
           console.log(res[j]);
+          putData(res[j], j);
         }
         console.log("OUT LOOP");
 
@@ -60,8 +89,8 @@ router.get("/", (req, res, next) => {
       console.log(":: END LOOP");
     }
   );
-
   res.render("search", {
+    bookimg: bookimg,
     title: title,
     writer: writer,
     publisher: publisher,
