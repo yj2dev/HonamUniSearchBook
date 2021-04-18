b01ec01b0ceb56b53b600c009dca7aba2272f7c6
Prodraft <prodraft@naver.com>
2021-04-18 13:46:49 +0900

    .

---
+++ BookCheck.py
@@ -0,0 +1,84 @@
+import requests
+import urllib.parse
+import json
+from bs4 import BeautifulSoup
+
+def bookSearchResultPage(keyword, searchType):
+  # api 주소
+  api = "http://library.honam.ac.kr/cheetah/api/search?"
+  # 세부검색 설정
+  values = {
+    "otwa1":searchType, # T = 도서명 / A = 저자명 / P = 출판사명 
+    "otod1": keyword,   # 검색할 문자열
+    "otbool1":"A",      # A = AND / O = OR  N = NOT
+    "otpn1":"K",        # K = 키워드 / L = 전방일치 / M = 완전일치
+    "sp":"1"            # 1 페이지 검색결과 < 페이지당 최대 10개밖에 안불러와짐 >
+
+    # << 여러 조건으로 검색할때 사용 / 아직 미제공 >>
+    #  "otwa2":"A",
+    #  "otod2": "",
+    #  "otbool2":"A",
+    #  "otpn2":"K",.
+
+    #  "otwa3":"P",   
+    #  "otod3": "",
+    #  "otbool3":"A",
+    #  "otpn3":"K",
+  # T = 서명 / TK = 서명 키워드 / A = 저자명 / AK = 저자명 키워드 / P = 출판사 / IDX = 통합 / S = 주제명 / SK = 주제명 키워드 / X = 총서명 / XK = 총서명 키워드 / N = ISBN,ISSN / NC = 청구기호 / R = 등록번호 
+
+  }
+
+  # 인코딩 후 링크 생성
+  params = urllib.parse.urlencode(values)
+  url = api + params
+
+  req = requests.get(url)
+  req = json.loads(req.text)
+  bookSearchData = [] # 검색결과 저장
+  for search in req["ListItem"]["BasicItem"]:
+    temp = []
+    bookTitle = search["Title"]           # 책 제목
+    author = search["Author"]             # 작가
+    publisher = search["Publisher"]       # 출판사
+    publishYear = search["PublishYear"]   # 책 출판년도
+    callNumber = search["CallNumber"]     # 책 청구기호
+    if callNumber != "":
+      callNumber = "("+callNumber+")"
+    cno = search["Cno"]                   # 호대 책관리 번호
+    bookImg = "http://library.honam.ac.kr/Cheetah/Shared/CoverImage?Cno=" + str(cno)
+    newBookTitle = bookTitle + "(" + str(publishYear) + ")"
+    # 대출정보 확인하기
+    bookRentalNum = 0
+    # 혹시모를 예외 방지 try문
+    try:
+      url2 = "https://library.honam.ac.kr/cheetah/Search/RnoSimpleList?Cno="+cno
+      req2 = requests.get(url2)
+      soup = BeautifulSoup(req2.text,'html.parser')
+      data = soup.select("tbody > tr")
+      bookWhere = "알수없음"
+
+      # 대출가능 권수 계산 ( 쌍촌분관에 있는 책은 제외 )
+      for bookData in data:
+        if (bookData.select_one("td:nth-child(3)").text.strip() != "쌍촌분관"):
+          bookWhere = bookData.select_one("td:nth-child(3)").text.strip()
+          if bookData.select_one("td:nth-child(4)").text.strip() == "대출가능":
+            bookRentalNum = bookRentalNum + 1
+      if bookRentalNum > 0:
+        isRental = str(bookRentalNum) + "권 대출가능"
+      else:
+        isRental = "대출불가"
+    except:
+      isRental = "대출불가"
+    bookLocation = bookWhere+callNumber
+    temp.append(bookImg)
+    temp.append(newBookTitle)
+    temp.append(author)
+    temp.append(publisher)
+    temp.append(bookLocation)
+    temp.append(isRental)
+    bookSearchData.append(temp)
+  return bookSearchData
+
+
+
+
--- main.py
+++ main.py
@@ -1,48 +1,72 @@
 from SearchBook import SearchLibraryBookPossession
 from BorrowBookInfo import SerchBorrowInfo
+from BookCheck import bookSearchResultPage
 
-# 도서관에서 책을 소장하고 있는지 검색
-print("책 제목을 입력하세요: ",end='')
-userInput = input()
-i = 1
-j = 1
-bookSearchInfo = SearchLibraryBookPossession(userInput,"T") # [0] 책 제목 ㅣ [1] 작가 ㅣ [2] 출판사 ㅣ [3] 책출판년도 ㅣ [4] 책관리번호 ㅣ [5] 책 이미지
 
-# 검색결과가 없을때
-if bookSearchInfo== []:
-  print("입력하신 책의 정보가 없습니다.")
 
-# 검색결과 출력
-else:
-  print("\n■도서관 소장자료 검색 결과 입니다.\n")
-  for serachResult in bookSearchInfo:
-    print("["+str(i)+"]책 제목 : "+ serachResult[0] + "(" + str(serachResult[3]) + ")")
-    print("["+str(i)+"]작가/출판사 : " + serachResult[1] + "/" + serachResult[2] )
-    print("")
-    i = i+1
 
-  # 책 대출가능여부 및 상세정보 확인
-  print("■도서 번호를 선택하세요: ",end='')
-  bookNum = int(input())
 
-  cno = bookSearchInfo[bookNum-1][4]
-  borrowInfoData = SerchBorrowInfo(cno) # [0] 대출가능여부 ㅣ [1] 책위치(몇층인지) ㅣ [2] 책위치(번호) ㅣ [3] 대출자학번(가림) ㅣ [4] 대출자이름(가림) ㅣ [5] 도서 반납 예정일 ㅣ [6] 대출자학번 ㅣ [7] 대출자이름
+
+data = bookSearchResultPage("해리포터","T")
+
+for bookInfo in data:
+  print("책이미지 : " + bookInfo[0])
+  print("책이름 : " + bookInfo[1])
+  print("지은이 : " + bookInfo[2])
+  print("출판사 : " + bookInfo[3])
+  print("소장위치 : " + bookInfo[4])
+  print("도서상태 : " + bookInfo[5])
+  print()
+
+
+
+
+
+
+
+# # 도서관에서 책을 소장하고 있는지 검색
+# print("책 제목을 입력하세요: ",end='')
+# userInput = input()
+# i = 1
+# j = 1
+# bookSearchInfo = SearchLibraryBookPossession(userInput,"T") # [0] 책 제목 ㅣ [1] 작가 ㅣ [2] 출판사 ㅣ [3] 책출판년도 ㅣ [4] 책관리번호 ㅣ [5] 책 이미지
+
+# # 검색결과가 없을때
+# if bookSearchInfo== []:
+#   print("입력하신 책의 정보가 없습니다.")
+
+# # 검색결과 출력
+# else:
+#   print("\n■도서관 소장자료 검색 결과 입니다.\n")
+#   for serachResult in bookSearchInfo:
+#     print("["+str(i)+"]책 제목 : "+ serachResult[0] + "(" + str(serachResult[3]) + ")")
+#     print("["+str(i)+"]작가/출판사 : " + serachResult[1] + "/" + serachResult[2] )
+#     print("")
+#     i = i+1
+
+#   # 책 대출가능여부 및 상세정보 확인
+#   print("■도서 번호를 선택하세요: ",end='')
+#   bookNum = int(input())
+
+#   cno = bookSearchInfo[bookNum-1][4]
+#   borrowInfoData = SerchBorrowInfo(cno) # [0] 대출가능여부 ㅣ [1] 책위치(몇층인지) ㅣ [2] 책위치(번호) ㅣ [3] 대출자학번(가림) ㅣ [4] 대출자이름(가림) ㅣ [5] 도서 반납 예정일 ㅣ [6] 대출자학번 ㅣ [7] 대출자이름
   
-  # 대출 정보가 안나오는책 
-  if borrowInfoData== []:
-    print("대출정보가 없는 책입니다. 같은 이름의 책이 여러개일 경우 다른책을 선택하세요.")
-  else:
-    for borrow in borrowInfoData:
-      # 책 상태가 대출가능일때 출력문
-      if borrow[0] =="대출가능":
-        print("["+str(j)+"]["+borrow[0]+"] 책위치: "+borrow[1]+"("+borrow[2]+")")
-      # 책이 대출중일때
-      else:
-        # 아직 서가에 비치되지 않은 책일경우
-        if "배가중" in borrow[7]:   
-          print("["+str(j)+"]["+borrow[0]+"] 아직 서가에 비치되지 않았습니다. ")
-        elif "진로개발센터" in borrow[7]:
-          print("["+str(j)+"]["+borrow[0]+"] 진로개발센터에서 소장중인 도서입니다. ")
-        else:
-          print("["+str(j)+"]["+borrow[0]+"] 대출자: "+borrow[6]+"("+borrow[7]+") 반납 예정일: "+borrow[5] )
-      j = j+1
+#   # 대출 정보가 안나오는책 
+#   if borrowInfoData== []:
+#     print("대출정보가 없는 책입니다. 같은 이름의 책이 여러개일 경우 다른책을 선택하세요.")
+#   else:
+#     for borrow in borrowInfoData:
+#       # 책 상태가 대출가능일때 출력문
+#       if borrow[0] =="대출가능":
+#         print("["+str(j)+"]["+borrow[0]+"] 책위치: "+borrow[1]+"("+borrow[2]+")")
+#       # 책이 대출중일때
+#       else:
+#         # 아직 서가에 비치되지 않은 책일경우
+#         if "배가중" in borrow[7]:   
+#           print("["+str(j)+"]["+borrow[0]+"] 아직 서가에 비치되지 않았습니다. ")
+#         elif "진로개발센터" in borrow[7]:
+#           print("["+str(j)+"]["+borrow[0]+"] 진로개발센터에서 소장중인 도서입니다. ")
+#         else:
+#           print("["+str(j)+"]["+borrow[0]+"] 대출자: "+borrow[6]+"("+borrow[7]+") 반납 예정일: "+borrow[5] )
+#       j = j+1
+
